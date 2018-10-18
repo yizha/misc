405600efe4f094c2108cf124e75f4ceb9597c9b5
Yicha Ding <ycding@freewheel.tv>
2018-10-18 11:31:17 +0800

    emacs.d/init.el update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,15 +11,24 @@
         color-theme-sanityinc-solarized
         exec-path-from-shell
         fill-column-indicator
+        company
+        company-go
         paredit
         smex
         go-mode))
 
 ;(dolist (package package-list) (unless (package-installed-p package) (package-install package)))
 
-(add-to-list 'custom-theme-load-path "/Users/ycding/.emacs.d/local/emacs-color-theme-solarized")
-(set-frame-parameter nil 'background-mode 'dark)
-(load-theme 'solarized t)
+(setq company-tooltip-limit 10)                      ; bigger popup window
+(setq company-idle-delay .5)                         ; decrease delay before autocompletion popup shows
+(setq company-echo-delay 0)                          ; remove annoying blinking
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+
+(add-hook 'go-mode-hook
+          (lambda ()
+            (set (make-local-variable 'company-backends) '(company-go))
+            (company-mode)
+            (local-set-key (kbd "C-c C-k") #'pop-tag-mark)))
 
 (require 'better-defaults)
 
@@ -36,7 +45,29 @@
 (set-face-attribute 'default nil :height 160)
 
 (when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "GOPATH"))
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (add-hook 'before-save-hook #'gofmt-before-save)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+ '(frame-background-mode (quote dark))
+ '(package-selected-packages
+   (quote
+    (smex paredit flycheck fill-column-indicator exec-path-from-shell company-go better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-to-list 'custom-theme-load-path "/Users/ycding/.emacs.d/local/emacs-color-theme-solarized")
+(load-theme 'solarized t)
