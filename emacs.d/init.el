4d8f5710ed1b368ce467966f4fc4876c3c7af30a
Yicha Ding <ycding@freewheel.tv>
2018-10-18 12:00:27 +0800

    emacs.d/init.el globally disable flycheck checker go-golink

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -58,6 +58,7 @@
  '(custom-safe-themes
    (quote
     ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+ '(flycheck-disabled-checkers (quote (go-golint)))
  '(frame-background-mode (quote dark))
  '(package-selected-packages
    (quote
